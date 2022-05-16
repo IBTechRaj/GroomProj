8b565eedc6715d08e64c4ade43f802ade80283e5
ibtechraj <krs30018@gmail.com>
2022-05-16 20:11:02 +0530

    date time appt tested other repo,about to add image to sprovider form

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,31 @@
 import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import axios from 'axios';
+// import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@mui/material/Grid'
+import Card from './Card'
+
+//https://st.depositphotos.com/1491329/2998/i/450/depositphotos_29983783-stock-photo-healthy-long-black-hair-beauty.jpg
+// https://static8.depositphotos.com/1491329/1068/i/450/depositphotos_10685520-stock-photo-hair-colors-palette.jpg
+// https://depositphotos.com/68471257/stock-photo-interior-of-empty-modern-hair.html
+// https://depositphotos.com/21503609/stock-photo-comb-brushes-hairdryer-and-cutting.html
+
+// import { DesktopDateTimePicker } from '@mui/x-date-pickers';cl
 // import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
+import { ClassNames } from '@emotion/react';
 // import SendMail from './SendMail';
 
-
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: '100px',
+    paddingRight: '20px'
+  }
+})
 function Landing({ loggedIn, setLoggedIn }) {
   // const jwt = localStorage.getItem('token');
+  const classes=useStyles()
   const salonurl = 'http://localhost:3001/salons';
   const serviceurl = 'http://localhost:3001/services';
 
@@ -33,7 +51,23 @@ function Landing({ loggedIn, setLoggedIn }) {
 
   return (
     <>
-      <section id="about">
+    <Grid container spacing={4} className={classes.gridContainer} justify="center">
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+    
+   
+    </Grid>
+      {/* <section id="about">
         <Container className="container">
           <Row className="row">
             <div className="section-header">
@@ -43,17 +77,17 @@ function Landing({ loggedIn, setLoggedIn }) {
           <Row >
 
             <Col xs={6} >
-              {/* <Image src="images/about.png" fluid /> */}
             </Col>
             <Col xs={6} >
               
               <p>
                 Salon pics scroll here horizontally
+                
               </p>
             </Col>
           </Row>
         </Container>
-      </section>
+      </section> */}
     </>
   )
 

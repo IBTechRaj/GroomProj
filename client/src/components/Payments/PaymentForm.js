a78bc3d006813fca1624a4936bd506b69c8dca30
ibtechraj <krs30018@gmail.com>
2022-07-16 13:25:41 +0530

    stripe branch for stripe server

---
+++ client/src/components/Payments/PaymentForm.js
@@ -0,0 +1,8 @@
+import { withRouter } from "react-router-dom";
+import { IdealBankElement, CardNumberElement } from "@stripe/react-stripe-js";
+
+const PaymentForm = () => {
+  return IdealBankElement
+}
+
+export default withRouter(PaymentForm)
